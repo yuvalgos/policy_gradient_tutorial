5d1d7354e09766ebaf8ed7cfc00e7086b29143ee
yuvalgos <yuvalgos@gmail.com>
2023-05-19 17:59:36 +0300

    library packing staff

--- __init__.py
+++ __init__.py
@@ -1,1 +1,5 @@
+# from pg_tutorial import utils as utils
+from pg_tutorial import parameterized_policy as parameterized_policy
+from pg_tutorial import parameter_space_visualization as parameter_space_visualization
+
 __version__ = "0.0.1"
